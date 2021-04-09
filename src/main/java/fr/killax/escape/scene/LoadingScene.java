1c336b28f14d536ed52156d0142359ef1d902deb
Killax-D <contact@dylan-donne.fr>
2021-04-09 19:43:02 +0200

    :sparkles: Add Tips to Loading Screen

--- src/main/java/fr/killax/escape/scene/LoadingScene.java
+++ src/main/java/fr/killax/escape/scene/LoadingScene.java
@@ -4,10 +4,15 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.Rectangle;
 import java.awt.geom.Rectangle2D;
 
 import fr.killax.escape.app.Config;
 import fr.killax.escape.assets.Assets;
+import fr.killax.escape.game.utils.Tips;
+import fr.killax.escape.utils.texts.TextAlignment;
+import fr.killax.escape.utils.texts.TextFormat;
+import fr.killax.escape.utils.texts.TextRenderer;
 
 public class LoadingScene extends AbstractScene {
 
@@ -15,6 +20,7 @@ public class LoadingScene extends AbstractScene {
 
 	private int loadingImageStep = 0;
 	private String loadingText = "Loading";
+	private String tip = Tips.getTip();
 	
 	@Override
 	public void update(double delta) {
@@ -37,6 +43,23 @@ public class LoadingScene extends AbstractScene {
 		g.drawImage(Assets.getTile("textures/ui/loading.png", 200, 200, this.loadingImageStep, 0).getScaledInstance(32, 32, Image.SCALE_FAST),
 				(int) Config.WINDOW_SIZE.getWidth() - 40,
 				(int) Config.WINDOW_SIZE.getHeight() - 40, null);
+
+		g.setColor(new Color(0.f, 0.f, 0.f, 0.75f));
+		g.fillRoundRect(20, (int) Config.WINDOW_SIZE.getHeight() - 200, (int) Config.WINDOW_SIZE.getWidth() - 40, 140, 25, 25);
+		g.setColor(new Color(1.f, 1.f, 1.f, 0.75f));
+		g.drawRoundRect(20, (int) Config.WINDOW_SIZE.getHeight() - 200, (int) Config.WINDOW_SIZE.getWidth() - 40, 140, 25, 25);
+		
+		Rectangle bounds = new Rectangle(40, (int) Config.WINDOW_SIZE.getHeight() - 180, (int) Config.WINDOW_SIZE.getWidth() - 80, 100);
+
+	    TextRenderer.drawString(
+	        g,
+	        this.tip,
+	        g.getFont(),
+	        Color.WHITE,
+	        bounds,
+	        TextAlignment.MIDDLE,
+	        TextFormat.FIRST_LINE_VISIBLE
+	    );
 		
 	}
 
